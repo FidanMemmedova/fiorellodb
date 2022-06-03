4dd4e6ea15ab3fd062e2585f284d35dfe5adece9
FidanMemmedova <5TAWDL9@code.edu.az>
2022-06-03 13:39:00 +0400

    first commit

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AdminPanel.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPanel.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {}
+        DbSet<Slide> Slides { get; set; }
+    }
+}
---
+++ Models/ProducImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPanel.Models
+{
+    public class ProducImage
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
