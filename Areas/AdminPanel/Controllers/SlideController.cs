720a43d6339ac88c02e90316c706974c8a4fa4d7
FidanMemmedova <5TAWDL9@code.edu.az>
2022-06-09 21:27:04 +0400

    bayaqdan baslamisam eslinde commit etmeyi unutmusam.

--- Areas/AdminPanel/Controllers/SlideController.cs
+++ Areas/AdminPanel/Controllers/SlideController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WEB.DAL;
+using WEB.Helpers;
 using WEB.Models;
 
 namespace WEB.Areas.AdminPanel.Controllers
@@ -13,9 +15,11 @@ namespace WEB.Areas.AdminPanel.Controllers
     public class SlideController : Controller
     {
         private AppDbContext _context { get; }
-        public SlideController(AppDbContext context)
+        private IWebHostEnvironment _env { get; }
+        public SlideController(AppDbContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
         public IActionResult Index()
         {
@@ -27,29 +31,52 @@ namespace WEB.Areas.AdminPanel.Controllers
         } 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Slide slide)
+        public async Task<IActionResult> Create(Slide slide)
         {
             if (!ModelState.IsValid)
             {
                 return View();
             }
-            if (slide.Photo.Length/1024>200)
+            if (!slide.Photo.CheckFileSize(200))
             {
                 ModelState.AddModelError("Photo", "Max size image must be less than 200kb");
                 return View();
             }
-            if (!slide.Photo.ContentType.Contains("image/"))
+            if (!slide.Photo.CheckFileType("image/"))
             { 
                 ModelState.AddModelError("Photo", "Type of file must be image");
                 return View();
             }
             //return Json(slide.Photo.ContentType.Contains("image/"));
             //return Json("Ok");
-            using (FileStream fileStream = new FileStream(@"C: \Users\ASUS\Desktop\fiorellodb\wwwroot\img" + slide.Photo.FileName, FileMode.Create))
-            {
-                slide.Photo.CopyTo(fileStream);
-            }
-            return Json(slide.Photo.FileName);
+           
+            slide.Url =await slide.Photo.SaveFileAsync(_env.WebRootPath,"img");
+            await _context.Slides.AddAsync(slide);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id==null)
+            {
+                return BadRequest();
+            }
+            var slider = _context.Slides.Find(id);
+            if (slider==null)
+            {
+                return NotFound();
+            }
+            var path = Helper.GetPath(_env.WebRootPath, "img", slider.Url);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            _context.Slides.Remove(slider);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+              
+        }
+        public async Task<IActionResult>Update()
     }
 }
