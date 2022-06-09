f330f0f56a8fbe5c98fc1bbc74e3283616dacdba
FidanMemmedova <5TAWDL9@code.edu.az>
2022-06-09 12:52:40 +0400

    saat 12:52

---
+++ Areas/AdminPanel/Controllers/SlideController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB.DAL;
+
+namespace WEB.Areas.AdminPanel.Controllers
+{
+    public class SlideController : Controller
+    {
+        private AppDbContext _context { get; }
+        public SlideController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Slides);
+        }
+    }
+}
